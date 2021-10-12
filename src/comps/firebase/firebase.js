592e2806a5631de0bfec574035ae6583cfcb4a86
Awaisp-irfan <irfanmaqsood.im123@gmail.com>
2021-10-13 03:46:10 +0500

    firebase setup

---
+++ src/comps/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdtKmafOrWqU8TbPjwDaKMG4Igigi5XNI",
+  authDomain: "photo-gallery-9c723.firebaseapp.com",
+  projectId: "photo-gallery-9c723",
+  storageBucket: "photo-gallery-9c723.appspot.com",
+  messagingSenderId: "173705488606",
+  appId: "1:173705488606:web:cf5b5dc1373cdb5cfc3a4f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestroage = firebase.storage()
+const db = firebase.firestore()
+export { firestroage,db}
